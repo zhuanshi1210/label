eebe1d99ff20ae0f0f9fb4fbdb37af163b278e21
leilei <thunderbey@163.com>
2019-09-20 11:11:13 +0800

    label1.0

--- labelweb/server/app.js
+++ labelweb/server/app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//配置跨域
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); //为了跨域保持session，所以指定地址，不能用*
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- labelweb/vueclient/src/main.js
+++ labelweb/vueclient/src/main.js
@@ -6,6 +6,13 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+//配置跨域
+import axios from 'axios';//引入axios组件
+axios.defaults.withCredentials=true;  //跨域保存session有用；由于是跨域请求，我们需要配置withCredentials为true，这样避免每次都被识别为新的请求。
+axios.defaults.baseURL = "http://localhost:3000"; //打包的时候直接删掉，默认的node服务器路径在这里配置
+Vue.prototype.$reqs = axios;  //将 axios 赋值给 Vue，方便在子组件里面使用
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
